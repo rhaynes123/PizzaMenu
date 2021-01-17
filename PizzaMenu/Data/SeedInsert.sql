83fab32b995c49e3e03cca5d67f3bb56d8c876da
Richard <rahayn@yahoo.com>
2021-01-16 19:19:20 -0500

    added seed file and changed title

---
+++ PizzaMenu/Data/SeedInsert.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO MenuItems(Name,Active,BasePrice,DateAdded, DateModified, DateDeactivated)
+VALUES('Pepperoni', 1,5.00,DateTime('now'),DateTime('now'),Null);
+
+INSERT INTO MenuItems(Name,Active,BasePrice,DateAdded, DateModified, DateDeactivated)
+VALUES('Bread Sticks', 1,1.99,DateTime('now'),DateTime('now'),Null);
