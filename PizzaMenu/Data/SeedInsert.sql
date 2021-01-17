f534ae1359e41a5888e58f823d6238b8c7b3af6d
Richard <rahayn@yahoo.com>
2021-01-17 01:21:51 -0500

    updated seed file

--- PizzaMenu/Data/SeedInsert.sql
+++ PizzaMenu/Data/SeedInsert.sql
@@ -1,5 +1,8 @@
-﻿INSERT INTO MenuItems(Name,Active,BasePrice,DateAdded, DateModified, DateDeactivated)
-VALUES('Pepperoni', 1,5.00,DateTime('now'),DateTime('now'),Null);
+﻿INSERT INTO MenuItems(Name,Active,BasePrice,DateAdded, DateModified, DateDeactivated,CookTime)
+VALUES('Pepperoni', 1,5.00,DateTime('now'),DateTime('now'),Null,3);
 
-INSERT INTO MenuItems(Name,Active,BasePrice,DateAdded, DateModified, DateDeactivated)
-VALUES('Bread Sticks', 1,1.99,DateTime('now'),DateTime('now'),Null);
+INSERT INTO MenuItems(Name,Active,BasePrice,DateAdded, DateModified, DateDeactivated, CookTime)
+VALUES('Bread Sticks', 1,1.99,DateTime('now'),DateTime('now'),Null,0);
+
+INSERT INTO MenuItems(Name,Active,BasePrice,DateAdded, DateModified, DateDeactivated,CookTime)
+VALUES('Wings', 1,3.99,DateTime('now'),DateTime('now'),Null,4);
